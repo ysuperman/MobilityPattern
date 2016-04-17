7638c7c0447b8e7a3ada92bd476ff31e22e40181
yougzhao <yougzhao@gmail.com>
2016-04-17 17:36:35 +0800

    add

--- src/Model/StayPoint.java
+++ src/Model/StayPoint.java
@@ -6,6 +6,7 @@ public class StayPoint{
 	public String sTime,eTime;
 	public int event;//信令数据事件位，暂时不用
 	public int state;//1：停留点；0：移动点
+	public int type;//停留点类型，0:未标定；1:居住地；2:工作地；3:休闲地；当且仅当state＝＝1时该位有意义。
 	public StayPoint(){
 		
 	}
