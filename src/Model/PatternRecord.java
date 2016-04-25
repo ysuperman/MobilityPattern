310bcfc56d4ebbfeed6b9f769b92ee3e2dc4a15e
yougzhao <yougzhao@gmail.com>
2016-04-25 08:43:29 +0800

    add

--- src/Model/PatternRecord.java
+++ src/Model/PatternRecord.java
@@ -1,5 +1,26 @@
 package Model;
+
+import java.util.LinkedList;
+import java.util.List;
+
 //用户模式点记录序列
 public class PatternRecord {
-
+	private String id;
+	private List<PatternPoint> patternPoints;
+	public PatternRecord(){
+		
+	}
+	public PatternRecord(String id){
+		this.id = id;
+		patternPoints = new LinkedList<PatternPoint>();
+	}
+	public void setId(String id){
+		this.id = id;
+	}
+	public String getId(){
+		return this.id;
+	}
+	public List<PatternPoint> getPatternPoints(){
+		return this.patternPoints;
+	}
 }
